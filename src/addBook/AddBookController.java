c6c0a65a95068c38188e07f0af7ac9e09e161f37
Deepak Dwivedi <deepak.dwivedi964@gmail.com>
2018-08-18 11:10:01 +0530

    DB Connected Using JDBC and Add Book Done.

--- src/addBook/AddBookController.java
+++ src/addBook/AddBookController.java
@@ -1,7 +1,92 @@
 package addBook;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
 
-public class AddBookController {
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ResourceBundle;
 
+import dbutils.ConnectDB;
+import javafx.event.ActionEvent;
+
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class AddBookController implements Initializable {
+	@FXML
+	private AnchorPane anchorPane;
+	@FXML
+	private TextField bookTitle;
+	@FXML
+	private TextField bookId;
+	@FXML
+	private TextField bookAuthor;
+	@FXML
+	private TextField bookPublisher;
+	
+	private ConnectDB connectDB;
+
+	// Event Listener on Button.onAction
+	@FXML
+	public void addBook(ActionEvent event) throws SQLException, InstantiationException, IllegalAccessException {
+		
+		String mtitle=bookTitle.getText();
+		String mid=bookId.getText();
+		String mauthor=bookAuthor.getText();
+		String mpublisher=bookPublisher.getText();
+		
+		if(mtitle.isEmpty()||mid.isEmpty()||mauthor.isEmpty()||mpublisher.isEmpty()) {
+			
+			Alert alert=new Alert(Alert.AlertType.ERROR);
+			alert.setHeaderText(null);
+			alert.setContentText("Fields must not Be empty");
+			alert.showAndWait();
+			return;
+			
+		}
+		
+		//here we perform Query
+		String sql="INSERT INTO BOOK (bookId,bookTitle,bookAuthor,bookPublisher) VALUES (?,?,?,?)";
+		Connection conn=ConnectDB.getConnection();
+		PreparedStatement pst= conn.prepareStatement(sql);
+		
+		pst.setString(1, mid);
+		pst.setString(2, mtitle);
+		pst.setString(3, mauthor);
+		pst.setString(4, mpublisher);
+		
+		pst.execute();
+		Alert alert=new Alert(Alert.AlertType.INFORMATION);
+		alert.setHeaderText(null);
+		alert.setContentText("Book Added Successfully");
+		alert.showAndWait();
+		Stage stage=(Stage) anchorPane.getScene().getWindow();
+		stage.close();
+		return;
+		
+		
+		
+		
+	}
+	// Event Listener on Button.onAction
+	@FXML
+	public void cancelBtn(ActionEvent event) {
+
+		Stage stage=(Stage) anchorPane.getScene().getWindow();
+		stage.close();
+
+		
+		
+	}
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		connectDB=new ConnectDB();
+		
+	}
 }
--- src/dbutils/ConnectDB.java
+++ src/dbutils/ConnectDB.java
@@ -4,15 +4,40 @@ import java.sql.*;
 
 public class ConnectDB {
  
-	 public static Connection getConnection() {
+	 @SuppressWarnings("deprecation")
+	public static Connection getConnection() throws SQLException, InstantiationException, IllegalAccessException {
+
+		 
+		 String url = "jdbc:mysql://localhost:3306/";
+
+	        /**
+	         * The MySQL user.
+	         */
+	        String user = "root";
+
+	        /**
+	         * Password for the above MySQL user. If no password has been 
+	         * set (as is the default for the root user in XAMPP's MySQL),
+	         * an empty string can be used.
+	         */
+	        String password = "";
+		 
+		 
 		 try {
-			Class.forName("org.sqlite.JDBC");
-			Connection conn=DriverManager.getConnection("jdbc:sqlite:""");		//Begin From Here
+			//Class.forName("com.mysql.jdbc.Driver");
+			
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+            Connection con = DriverManager.getConnection(url, user, password);
+            Statement stt = con.createStatement();
+    		stt.execute("USE libraryManagementSystem");
+			//Connection conn=DriverManager.getConnection("jdbc:mysql://127.0.0.1//libraryManagementSystem");	//may change to localhost
+			return con;//Begin From Here
 			
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		 
+		 return null;
 	 }
 }
